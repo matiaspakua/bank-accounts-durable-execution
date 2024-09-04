a075a22930a96e71381857fae8725658267c28be
matiaspakua <matiaspakua@gmail.com>
2024-09-04 22:11:02 +0200

    initial commit

---
+++ src/main/java/com/matiaspakua/bank_accounts_durable_execution/BankAccountsDurableExecutionApplication.java
@@ -0,0 +1,13 @@
+package com.matiaspakua.bank_accounts_durable_execution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountsDurableExecutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountsDurableExecutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matiaspakua/bank_accounts_durable_execution/BankAccountsDurableExecutionApplicationTests.java
@@ -0,0 +1,15 @@
+package com.matiaspakua.bank_accounts_durable_execution;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountsDurableExecutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertTrue(true);
+	}
+
+}
